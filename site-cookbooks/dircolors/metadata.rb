b06ede5b7ac128227683aaf645e2fe951ab8d6b7
Allan Espinosa <allan.espinosa@outlook.com>
2012-11-23 00:01:06 +0900

    Sanitize cookbook metadata

--- site-cookbooks/dircolors/metadata.rb
+++ site-cookbooks/dircolors/metadata.rb
@@ -1,6 +1,8 @@
+name             "dircolors"
 maintainer       "Allan Espinosa"
 maintainer_email "allan.espinosa@outlook.com"
-license          "All rights reserved"
-description      "Installs/Configures dircolors"
+license          "Apache 2.0"
+description      "Installs dircolors with the solarized colorscheme"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+recipe           "dircolors", "Installs the the solarized dark colorscheme for LS_COLORS"
